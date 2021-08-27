d899941f23ff7807990b0051cc134c37fe550bce
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-27 16:58:58 +0000

    Add new message event and listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Events\NewMessageEvent;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        NewMessageEvent::class => [
+            \App\Listeners\SendMessageListener::class,
+        ]
     ];
 
     /**
