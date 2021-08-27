e2bdb32698b91bc3f5fd3cfb47f7c4cb6251a369
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-27 17:01:05 +0000

    Add user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(): View
+    {
+        return view('users.index', [
+            'users' => User::all(),
+        ]);
+    }
+
+    public function create(): View
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        User::create($request->all());
+        return redirect()->route('users.index');
+    }
+
+    public function show(User $user): View
+    {
+        return view('users.show', [
+            'user' => $user
+        ]);
+    }
+
+    public function edit(User $user): View
+    {
+        return view('users.edit', [
+            'user' => $user,
+        ]);
+    }
+
+    public function update(Request $request, User $user): RedirectResponse
+    {
+        $user->update($request->all());
+        return redirect()->route('users.index');
+    }
+
+    public function destroy(User $user): RedirectResponse
+    {
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+
+    public function notifications(User $user): View
+    {
+        $user->unreadNotifications->markAsRead();
+
+        return view('users.notifications', [
+            'notifications' => $user->notifications
+        ]);
+    }
+}
