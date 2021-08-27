41fc48f5a223128bff8713f6f69baa0bd4932f0d
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-27 04:15:51 +0000

    Install translatable

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 `composer require laravel/fortify`
 > install package for localization
 `composer require mcamara/laravel-localization`
+> install package to make models translatable
+`composer require spatie/laravel-translatable`
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Translatable\HasTranslations;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasTranslations;
+
+    public array $translatable = [
+        'name',
+    ];
 
     /**
      * The attributes that are mass assignable.
