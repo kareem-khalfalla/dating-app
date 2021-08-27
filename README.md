28c8c9090bc4c0af652f42cd565d2bf547366b7c
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-27 04:01:11 +0000

    Install mcamara

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 > install package for authenticate
 `composer require laravel/fortify`
+> install package for localization
+`composer require mcamara/laravel-localization`
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,15 @@
         </style>
     </head>
     <body class="antialiased">
+        <ul>
+            @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                <li>
+                    <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                        {{ $properties['native'] }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
