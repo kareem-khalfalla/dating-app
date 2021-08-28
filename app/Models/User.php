012582409badae7778a40738429080d56c7918b8
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-28 13:24:19 +0000

    Add username to fillable, set profile relation set username as route key name

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -26,6 +28,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'last_login',
     ];
 
     /**
@@ -46,4 +50,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function scopeFilterByName(Builder $builder): Builder
+    {
+        $query = request('query');
+        return $builder->where('name', 'like', "%$query%");
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'username';
+    }
 }
