69ebf9d81ac070bd715dce1f0b13de3bb1de1178
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-30 20:38:25 +0000

    Add cities and states route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::group(
     [
@@ -37,3 +26,6 @@ Route::group(
         Route::get('{user}/message-request-refused', [ProfileController::class, 'messageRequestRefused'])->name('profiles.messageRequestRefused');
     }
 );
+
+Route::get('cities', [CityController::class, 'index'])->name('cities.index');
+Route::get('states', [CityController::class, 'index'])->name('states.index');
