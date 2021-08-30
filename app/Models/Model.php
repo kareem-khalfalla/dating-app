8b065336df4e357fbd5c9c65d87db57c4859d3c2
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-30 20:33:33 +0000

    Add abstract models

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
-class Model extends BaseModel
+abstract class Model extends BaseModel
 {
     use HasFactory;
 
---
+++ app/Models/ModelTranslated.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Translatable\HasTranslations;
+
+abstract class ModelTranslated extends Model
+{
+    use HasTranslations;
+
+    public array $translatable = [
+        'name'
+    ];
+}
