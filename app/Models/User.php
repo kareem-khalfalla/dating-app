6e7f68da51563c61997e589e9252b585aa400a9a
Kareem <kareem.khalfalla.dev@gmail.com>
2021-08-31 00:40:37 +0000

    Implements must verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Spatie\Translatable\HasTranslations;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasTranslations, HasRoles;
 
