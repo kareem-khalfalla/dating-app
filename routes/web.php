200e68d83f2f9d7b4cf4b6821533bfc0d3cc8a5d
Kareem <kareem.khalfalla.dev@gmail.com>
2021-09-02 23:10:51 +0000

    App routes

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,30 @@
 <?php
 
-use App\Http\Controllers\CityController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('search', function () {
+    return view('search');
+})->name('search');
 
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => ['auth', 'verified', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+        'middleware' => ['auth', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
     ],
     function () {
-        Route::get('/', function () {
-            return view('welcome');
-        });
 
         Route::get('results', [UserController::class, 'filter'])->name('users.filter');
-        Route::get('{user}', [ProfileController::class, 'show'])->name('users.profile');
-        Route::get('{user}/message-request', [ProfileController::class, 'messageRequest'])->name('profiles.messageRequest');
+        Route::get('{user}', [ProfileController::class, 'index'])->name('profile');
+        Route::get('{user}/settings', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::get('{user}/message-request', [ProfileController::class, 'messageRequest'])->name('messageRequest');
         Route::get('{user}/notifications', [UserController::class, 'notifications'])->name('users.notifications');
         Route::get('{user}/message-request-refused', [ProfileController::class, 'messageRequestRefused'])->name('profiles.messageRequestRefused');
     }
 );
-
-Route::get('cities', [CityController::class, 'index'])->name('cities.index');
-Route::get('states', [CityController::class, 'index'])->name('states.index');
