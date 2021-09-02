3776e07ec7f1791d21515ee8e1da6763033680fd
Kareem <kareem.khalfalla.dev@gmail.com>
2021-09-02 23:11:28 +0000

    Return view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,71 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\MessageRequestEvent;
-use App\Events\MessageRequestRefusedEvent;
-use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index(): View
+    public function index(User $user): View
     {
-        return view('profiles.index', [
-            'profiles' => Profile::all(),
+        return view('pages.profiles.index', [
+            'user' => $user,
         ]);
     }
 
-    public function create(): View
+    public function edit(User $user): View
     {
-        return view('profiles.create');
-    }
-
-    public function store(Request $request): RedirectResponse
-    {
-        Profile::create($request->all());
-        return redirect()->route('profiles.index');
-    }
-
-    public function show(User $user): View
-    {
-        return view('profiles.show', [
-            'user' => $user
+        return view('pages.profiles.edit', [
+            'user' => $user,
         ]);
-    }
-
-    public function edit(Profile $profile): View
-    {
-        return view('profiles.edit', [
-            'profile' => $profile,
-        ]);
-    }
-
-    public function update(Request $request, Profile $profile): RedirectResponse
-    {
-        $profile->update($request->all());
-        return redirect()->route('profiles.index');
-    }
-
-    public function destroy(Profile $profile): RedirectResponse
-    {
-        $profile->delete();
-        return redirect()->route('profiles.index');
-    }
-
-    public function messageRequest(User $user): RedirectResponse
-    {
-        event(new MessageRequestEvent($user));
-        
-        return redirect()->back();
-    }
-
-    public function messageRequestRefused(User $user): RedirectResponse
-    {
-        event(new MessageRequestRefusedEvent($user));
-        
-        return redirect()->back();
     }
 }
