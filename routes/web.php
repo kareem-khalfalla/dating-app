64f24b81093fb2518f7ad9d5dcead7c1ffc6422e
Kareem <kareem.khalfalla.dev@gmail.com>
2021-09-03 12:13:00 +0000

    app routes

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('search', function () {
-    return view('search');
-})->name('search');
+// Route::get('search', function () {
+//     return view('search');
+// })->name('search');
 
+Route::get('settings', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => ['auth', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
     ],
     function () {
-
         Route::get('results', [UserController::class, 'filter'])->name('users.filter');
         Route::get('{user}', [ProfileController::class, 'index'])->name('profile');
-        Route::get('{user}/settings', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::get('{user}/message-request', [ProfileController::class, 'messageRequest'])->name('messageRequest');
         Route::get('{user}/notifications', [UserController::class, 'notifications'])->name('users.notifications');
         Route::get('{user}/message-request-refused', [ProfileController::class, 'messageRequestRefused'])->name('profiles.messageRequestRefused');
