25afde50df8802219938353d744bf74ae5be899a
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-18 07:44:31 +0200

    remove unused code

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use App\Traits\Friendable;
+use Hootlex\Friendships\Traits\Friendable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
