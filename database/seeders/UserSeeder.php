ffdc70218c276546357c07300d8980155dc79841
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-19 07:56:41 +0200

    add languages to user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,6 +23,8 @@ use App\Models\HairLength;
 use App\Models\HalalFoodStatus;
 use App\Models\Headdress;
 use App\Models\HealthStatus;
+use App\Models\Language;
+use App\Models\LanguagePerfectionStatus;
 use App\Models\MaritalStatus;
 use App\Models\MarriageStatus;
 use App\Models\MusicStatus;
@@ -52,7 +54,6 @@ use App\Models\WifeStudyStatus;
 use App\Models\WifeWorkStatus;
 use App\Models\WorkStatus;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -63,17 +64,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
-            'name' => Str::random(),
-            'email' => Str::random(10). '@gmail.com',
-            'username' => Str::random(),
-            'phone' => Str::random(5). '123-123-123',
-            'password' => bcrypt('secret'),
-            'gender' => 'male',
+        $user = User::factory()->create();
+        $languageNative = Language::all()->random();
+        $languageSecond = Language::all()->random();
+        $languageThird = Language::all()->random();
+        $languagePerfectionStatus = LanguagePerfectionStatus::all()->random();
+        $languageNative->update([
+            'language_perfection_status_id' => $languagePerfectionStatus->id
+        ]);
+        $languageSecond->update([
+            'language_perfection_status_id' => $languagePerfectionStatus->id,
+            'order' => 2
+        ]);
+        $languageThird->update([
+            'language_perfection_status_id' => $languagePerfectionStatus->id,
+            'order' => 3
         ]);
 
         /** @var \App\Models\Profile $adminUserProfile */
         $adminUserProfile = $user->profile()->create();
+        $adminUserProfile->languages()->attach($languageNative);
+        $adminUserProfile->languages()->attach($languageSecond);
+        $adminUserProfile->languages()->attach($languageThird);
         $adminUserProfile->education_status_id = EducationStatus::all()->random()->id;
         $adminUserProfile->work_status_id = WorkStatus::all()->random()->id;
         $adminUserProfile->marriage_status_id = MarriageStatus::all()->random()->id;
@@ -88,20 +100,17 @@ class UserSeeder extends Seeder
         $adminUserProfile->hometown_id = Country::all()->random()->id;
         $adminUserProfile->country_of_residence_id = Country::all()->random()->id;
         $adminUserProfile->state_id = State::all()->random()->id;
-
         $adminUserProfile->specialization = 'Computers';
-        $adminUserProfile->income = 999.99;
+        $adminUserProfile->income = rand(999, 9999);
         $adminUserProfile->bio = 'Lorem ipsum dolor sit amet consectetur.';
         $adminUserProfile->partner_bio = 'Lorem, ipsum dolor sit amet consectetur adipisicing.';
         $adminUserProfile->competence = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.';
         $adminUserProfile->dob = '1995-01-14';
         $adminUserProfile->postal_code = '123-45';
         $adminUserProfile->progress_bar = '99.99';
-
         $adminUserProfile->shelter_type_id = ShelterType::all()->random()->id;
         $adminUserProfile->shelter_shape_id = ShelterShape::all()->random()->id;
         $adminUserProfile->shelter_way_id = ShelterWay::all()->random()->id;
-
         $adminUserProfile->smoke_status_id = SmokeStatus::all()->random()->id;
         $adminUserProfile->alcohol_status_id = AlcoholStatus::all()->random()->id;
         $adminUserProfile->halal_food_status_id = HalalFoodStatus::all()->random()->id;
@@ -109,7 +118,6 @@ class UserSeeder extends Seeder
         $adminUserProfile->books = 'Lorem ipsum, dolor sit amet consectetur adipisicing.';
         $adminUserProfile->places = 'Lorem ipsum dolor sit amet consectetur.';
         $adminUserProfile->interests = 'Lorem ipsum dolor sit amet.';
-
         $adminUserProfile->body_status_id = BodyStatus::all()->random()->id;
         $adminUserProfile->skin_status_id = SkinStatus::all()->random()->id;
         $adminUserProfile->hair_color_id = HairColor::all()->random()->id;
@@ -122,22 +130,16 @@ class UserSeeder extends Seeder
         $adminUserProfile->height = rand(150, 200);
         $adminUserProfile->weight = rand(50, 150);
         $adminUserProfile->clarification = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsam?';
-
         $adminUserProfile->marital_status_id = MaritalStatus::all()->random()->id;
         $adminUserProfile->polygamy_status_id = PolygamyStatus::all()->random()->id;
         $adminUserProfile->shelter_type_id = ShelterType::all()->random()->id;
         $adminUserProfile->shelter_shape_id = ShelterShape::all()->random()->id;
         $adminUserProfile->shelter_way_id = ShelterWay::all()->random()->id;
-
         $adminUserProfile->children_count = rand(1, 9);
         $adminUserProfile->children_status_id = ChildrenStatus::all()->random()->id;
         $adminUserProfile->children_information = 'Lorem ipsum dolor sit.';
-
         $adminUserProfile->children_desire_status_id = ChildrenDesireStatus::all()->random()->id;
         $adminUserProfile->divorced_reason = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima error iste cumque omnis iure. Ratione!';
-
-
-
         $adminUserProfile->religion_id = Religion::all()->random()->id;
         $adminUserProfile->religion_method_id = ReligionMethod::all()->random()->id;
         $adminUserProfile->obligation_id = Obligation::all()->random()->id;
@@ -155,7 +157,6 @@ class UserSeeder extends Seeder
         $adminUserProfile->friend_status_id = FriendStatus::all()->random()->id;
         $adminUserProfile->show_status_id = ShowStatus::all()->random()->id;
         $adminUserProfile->lesson_listing = 'Lorem ipsum dolor sit amet.';
-
         $adminUserProfile->save();
     }
 }
