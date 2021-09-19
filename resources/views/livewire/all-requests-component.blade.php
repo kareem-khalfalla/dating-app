3a244731adddb950c7452726f94eab7a6f6f3dc9
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-19 13:11:54 +0200

    add profile url

--- resources/views/livewire/all-requests-component.blade.php
+++ resources/views/livewire/all-requests-component.blade.php
@@ -4,15 +4,16 @@
     </div>
     <div class="card-body">
         @forelse ($requests as $request)
+            @php
+                $user = \App\Models\User::find($request->sender_id);
+            @endphp
             <div class="row box_frind col-12 p-1">
-                <a href="profile-id.html">
-                    <img class="img_user"
-                        src="{{ asset('storage/' . \App\Models\User::find($request->sender_id)->avatar) }}"
-                        alt="{{ \App\Models\User::find($request->sender_id)->name }}"
-                        >
+                <a href="{{ route('profile', $user) }}">
+                    <img class="img_user" src="{{ asset('storage/' . $user->avatar) }}"
+                        alt="{{ $user->name }}">
                 </a>
                 <h5 class="col-5">
-                    {{ \App\Models\User::find($request->sender_id)->name }}
+                    {{ $user->name }}
                 </h5>
                 <a wire:click.prevent="accept({{ $request->sender_id }})"><button
                         class="btn btn-outline-primary">accept</button></a>&nbsp;
