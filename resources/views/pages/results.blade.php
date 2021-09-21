2b377a78628fb84722ab067fb9ae9f086f404445
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-22 00:09:46 +0200

    add to the main layout more data

--- resources/views/pages/results.blade.php
+++ resources/views/pages/results.blade.php
@@ -1,3 +1,22 @@
 <x-app-layout>
+    @push('head')
+        <meta name="theme-color" content="#5f023a" />
+        <meta name="msapplication-navbutton-color" content="#5f023a" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#5f023a" />
+    @endpush
+    @push('styles')
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    @endpush
+    <br><br>
     @livewire('users-filter-component')
+    <br><br>
+    <br><br>
+    @push('scripts')
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+        <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
+    @endpush
 </x-app-layout>
