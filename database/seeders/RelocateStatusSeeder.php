05ebc4a395259141554756dd7fddbf5b783b9d6e
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-23 11:46:27 +0200

    fix hometown to be origin instead

--- database/seeders/RelocateStatusSeeder.php
+++ database/seeders/RelocateStatusSeeder.php
@@ -18,7 +18,7 @@ class RelocateStatusSeeder extends Seeder
             ['name' => ['en' => 'Accept', 'ar' => 'أقبل', 'de' => 'Ich stimme zu']],
             ['name' => ['en' => 'Refuse', 'ar' => 'ﻻ أقبل', 'de' => 'Ich stimme nicht zu']],
             ['name' => ['en' => 'Accept due to nearby city', 'ar' => 'أقبل الى مدينة قريبة', 'de' => 'Stadt in der Nähe zu akzeptieren']],
-            ['name' => ['en' => 'Accept due to be inside my hometown', 'ar' => 'أقبل داخل دولتي', 'de' => 'Ich komme in mein Land']],
+            ['name' => ['en' => 'Accept due to be inside my origin', 'ar' => 'أقبل داخل دولتي', 'de' => 'Ich komme in mein Land']],
             ['name' => ['en' => 'Accept due to nearby country', 'ar' => 'أقبل الى دولة قريبة', 'de' => 'In ein nahegelegenes Land kommen']],
         ];
 
