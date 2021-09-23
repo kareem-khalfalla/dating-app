060ebb9de36f067df017cfaa957b5c21b4e3e3d4
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-09-23 14:51:06 +0200

    fix country model relations

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,28 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
 class Country extends Model
 {
-    public function profiles(): BelongsToMany
-    {
-        return $this->belongsToMany(Profile::class)->withPivot('is_hometown');
-    }
-
-    public function scopeHometown(Builder $query): Builder
-    {
-        return $query->where('is_hometown', 1);
-    }
-
-    public function scopeResidence(Builder $query): Builder
-    {
-        return $query->where('is_hometown', 0);
-    }
-
     public function getTranslationsAttribute(string $attr): array
     {
         return json_decode($attr, true);
+    }
+
+    public function getCountryLocale(): string
+    {
+        $locale = app()->getLocale() == 'ar'
+            ? 'fa'
+            : app()->getLocale();
+
+        return $this->translations[$locale] ?? $this->name;
     }
 }
