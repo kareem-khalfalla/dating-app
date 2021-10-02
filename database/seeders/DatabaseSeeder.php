723e90d90ee5868ad875921e9589590a04e40e49
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-10-02 19:45:14 +0200

    no fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // CountrySeeder::class,
-            // StateSeeder::class,
-            // NationalitySeeder::class,
-            // LanguageSeeder::class,
+            CountrySeeder::class,
+            StateSeeder::class,
+            NationalitySeeder::class,
+            LanguageSeeder::class,
             // UserSeeder::class,
         ]);
     }
