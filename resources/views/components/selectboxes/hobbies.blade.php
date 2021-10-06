c1d665812c9ca365559350fb6fada08a8b4912ea
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-10-06 08:35:28 +0200

    fix isset problem

--- resources/views/components/selectboxes/hobbies.blade.php
+++ resources/views/components/selectboxes/hobbies.blade.php
@@ -1,24 +1,24 @@
-<option value="Sport" {{ in_array('Sport', $hobbies) ? 'selected' : '' }}>{{ __('data.Sport') }}</option>
-<option value="Reading" {{ in_array('Reading', $hobbies) ? 'selected' : '' }}>{{ __('data.Reading') }}</option>
-<option value="Writing" {{ in_array('Writing', $hobbies) ? 'selected' : '' }}>{{ __('data.Writing') }}</option>
-<option value="Travel" {{ in_array('Travel', $hobbies) ? 'selected' : '' }}>{{ __('data.Travel') }}</option>
-<option value="Games" {{ in_array('Games', $hobbies) ? 'selected' : '' }}>{{ __('data.Games') }}</option>
-<option value="Computer" {{ in_array('Computer', $hobbies) ? 'selected' : '' }}>{{ __('data.Computer') }}</option>
-<option value="Spoking" {{ in_array('Spoking', $hobbies) ? 'selected' : '' }}>{{ __('data.Spoking') }}</option>
-<option value="Go to cafe" {{ in_array('Go to cafe', $hobbies) ? 'selected' : '' }}>{{ __('data.Go to cafe') }}
+<option value="Sport" {{ isset($hobbies) && in_array('Sport', $hobbies) ? 'selected' : '' }}>{{ __('data.Sport') }}</option>
+<option value="Reading" {{ isset($hobbies) && in_array('Reading', $hobbies) ? 'selected' : '' }}>{{ __('data.Reading') }}</option>
+<option value="Writing" {{ isset($hobbies) && in_array('Writing', $hobbies) ? 'selected' : '' }}>{{ __('data.Writing') }}</option>
+<option value="Travel" {{ isset($hobbies) && in_array('Travel', $hobbies) ? 'selected' : '' }}>{{ __('data.Travel') }}</option>
+<option value="Games" {{ isset($hobbies) && in_array('Games', $hobbies) ? 'selected' : '' }}>{{ __('data.Games') }}</option>
+<option value="Computer" {{ isset($hobbies) && in_array('Computer', $hobbies) ? 'selected' : '' }}>{{ __('data.Computer') }}</option>
+<option value="Spoking" {{ isset($hobbies) && in_array('Spoking', $hobbies) ? 'selected' : '' }}>{{ __('data.Spoking') }}</option>
+<option value="Go to cafe" {{ isset($hobbies) && in_array('Go to cafe', $hobbies) ? 'selected' : '' }}>{{ __('data.Go to cafe') }}
 </option>
-<option value="Rough games" {{ in_array('Rough games', $hobbies) ? 'selected' : '' }}>{{ __('data.Rough games') }}
+<option value="Rough games" {{ isset($hobbies) && in_array('Rough games', $hobbies) ? 'selected' : '' }}>{{ __('data.Rough games') }}
 </option>
-<option value="Chess" {{ in_array('Chess', $hobbies) ? 'selected' : '' }}>{{ __('data.Chess') }}</option>
-<option value="Disko" {{ in_array('Disko', $hobbies) ? 'selected' : '' }}>{{ __('data.Disko') }}</option>
-<option value="Stay at natural" {{ in_array('Stay at natural', $hobbies) ? 'selected' : '' }}>
+<option value="Chess" {{ isset($hobbies) && in_array('Chess', $hobbies) ? 'selected' : '' }}>{{ __('data.Chess') }}</option>
+<option value="Disko" {{ isset($hobbies) && in_array('Disko', $hobbies) ? 'selected' : '' }}>{{ __('data.Disko') }}</option>
+<option value="Stay at natural" {{ isset($hobbies) && in_array('Stay at natural', $hobbies) ? 'selected' : '' }}>
     {{ __('data.Stay at natural') }}</option>
-<option value="Action games" {{ in_array('Action games', $hobbies) ? 'selected' : '' }}>
+<option value="Action games" {{ isset($hobbies) && in_array('Action games', $hobbies) ? 'selected' : '' }}>
     {{ __('data.Action games') }}</option>
-<option value="Watching movies" {{ in_array('Watching movies', $hobbies) ? 'selected' : '' }}>
+<option value="Watching movies" {{ isset($hobbies) && in_array('Watching movies', $hobbies) ? 'selected' : '' }}>
     {{ __('data.Watching movies') }}</option>
-<option value="Watching cartoon movies" {{ in_array('Watching cartoon movies', $hobbies) ? 'selected' : '' }}>
+<option value="Watching cartoon movies" {{ isset($hobbies) && in_array('Watching cartoon movies', $hobbies) ? 'selected' : '' }}>
     {{ __('data.Watching cartoon movies') }}</option>
-<option value="going out With friends" {{ in_array('going out With friends', $hobbies) ? 'selected' : '' }}>
+<option value="going out With friends" {{ isset($hobbies) && in_array('going out With friends', $hobbies) ? 'selected' : '' }}>
     {{ __('data.going out With friends') }}</option>
-<option value="Walking" {{ in_array('Walking', $hobbies) ? 'selected' : '' }}>{{ __('data.Walking') }}</option>
+<option value="Walking" {{ isset($hobbies) && in_array('Walking', $hobbies) ? 'selected' : '' }}>{{ __('data.Walking') }}</option>
