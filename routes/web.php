db53c36311ddce1aa7c133c6eab5e18bc9e321f0
kareem <kareem.khalfalla.dev@gmail.com>
2021-10-12 18:42:21 +0200

    add verified middleware

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('privacy', [SiteController::class, 'privacy'])->name('privacy');
 Route::post('contact-us', [SiteController::class, 'contactStore'])->name('contactStore');
 Route::get('not-allowed', [SiteController::class, 'notAllowed'])->name('notAllowed');
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('chat', [SiteController::class, 'chat'])->name('chat')->middleware('verifyFriendsCount');
     Route::get('friends', [SiteController::class, 'friends'])->name('friends');
     Route::get('results', [UserController::class, 'results'])->name('results');
