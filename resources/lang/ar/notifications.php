b41e989e5d69991ea930bec5b180fce236b279aa
kareem <kareem.khalfalla.dev@gmail.com>
2021-10-22 20:53:33 +0200

    fix locale

--- resources/lang/ar/notifications.php
+++ resources/lang/ar/notifications.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'Congratulations, your friend request has been accepted' => 'مبروك ، لقد قبلت طلب الصداقة الخاص بك.',
+    'Congratulations, your friend request has been accepted' => 'مبروك ، لقد تم قبول طلب الصداقة الخاص بك.',
     'Sadlly, your request has been rejected'                    => 'للأسف ،رفض طلبك.',
     'New friend request received'              => 'طلب صداقة جديد',
 ];
