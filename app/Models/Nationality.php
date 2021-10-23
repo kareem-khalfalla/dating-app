db6624d966b1cf1846b8b87abb956ec56d828c5d
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-10-23 01:16:49 +0000

    rm spatie package and handle name

--- app/Models/Nationality.php
+++ app/Models/Nationality.php
@@ -2,7 +2,17 @@
 
 namespace App\Models;
 
-class Nationality extends ModelTranslated
+class Nationality extends Model
 {
-    // 
+    public function getName(): string
+    {
+        $locale = app()->getLocale();
+        $langsAvailable = array_keys(json_decode($this->name, true));
+        
+        if (in_array($locale, $langsAvailable)) {
+            return json_decode($this->name, true)[$locale];
+        } else {
+            return json_decode($this->name, true)['en'];
+        }
+    }
 }
