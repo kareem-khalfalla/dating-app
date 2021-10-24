d0caa9620de13fee188c4aed07fcaf13b4e96aea
Kareem Khalfalla <kareem.khalfalla.dev@gmail.com>
2021-10-24 03:39:16 +0000

    fix ar notification

--- resources/lang/ar/notifications.php
+++ resources/lang/ar/notifications.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'Congratulations, :username has been accepted your friend request' => 'مبروك, :username قد قبل طلب صداقتك',
+    'Congratulations, :username has been accepted your friend request' => 'قد تم قبول صداقتك من قبل :username',
     'Sadlly, :username your request has been rejected'                 => 'للأسف :username ،رفض طلبك.',
     'New friend request received from :username'                       => 'طلب صداقة جديد من :username',
 ];
